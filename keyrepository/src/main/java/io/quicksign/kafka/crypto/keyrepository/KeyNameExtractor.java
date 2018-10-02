fc8304398491717f5ecf58cc2439605f50ce89a3
Julien Vaudour <julien.vaudour@quicksign.com>
2018-10-02 19:59:41 +0200

    add keyrepository based implementation (for example a third party vault

---
+++ keyrepository/src/main/java/io/quicksign/kafka/crypto/keyrepository/KeyNameExtractor.java
@@ -0,0 +1,28 @@
+/*-
+ * #%L
+ * Kafka Encryption
+ * %%
+ * Copyright (C) 2018 Quicksign
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package io.quicksign.kafka.crypto.keyrepository;
+
+/**
+ * Used at encryption time to infer key name from topic name and message key
+ */
+public interface KeyNameExtractor {
+
+    String extractKeyName(String topic, Object key);
+}
